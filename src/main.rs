bf32c61b62f0a756dfa957c8f4b0a0fc53b9837f
Shane Osbourne <shane.osbourne8@gmail.com>
2018-07-30 18:16:46 +0100

    set host header

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use bytes::{Bytes};
 use futures::{Future};
 use std::sync::Arc;
 use std::net::SocketAddr;
+use actix_web::http::header;
 
 fn main() {}
 
@@ -16,50 +17,61 @@ const STR: &str = "Hello world";
 
 #[test]
 fn test_body() {
-    let mut srv = test::TestServer::new(|app| app.handler(|req: &HttpRequest| {
-        println!("headers received: {:?}", req.headers());
+    let server = test::TestServer::new(|app| app.handler(|req: &HttpRequest| {
+        println!("headers received at proxy addr: {:#?}", req.headers());
         HttpResponse::Ok()
             .header("shane", "kittens")
             .body(STR)
     }));
 
-    let srv_address = match srv.addr() {
-        SocketAddr::V4(ip) => ip.to_string(),
-        _ => "".to_string()
-    };
-
-    let srv_address = Arc::new(srv_address);
+    let srv_address = server.addr().to_string();
+    println!("orig address = {}", srv_address);
 
     let mut proxy = test::TestServer::new(move |app| {
-        let clone = Arc::clone(&srv_address);
+
+        let addr = srv_address.clone();
+
         app.handler(move |_req: &HttpRequest| -> Box<Future<Item = HttpResponse, Error = Error>> {
-            println!("proxy handler: {:?}", _req.headers());
-            client::ClientRequest::get(format!("http://{}", clone).as_str())
-                .finish().unwrap()
+            println!("proxy handler: {:#?}", _req.headers());
+
+            let mut outgoing = client::ClientRequest::get(format!("http://{}", addr.clone()).as_str());
+
+            for (key, value) in _req.headers() {
+                outgoing.header(key.clone(), value.clone());
+            }
+
+            outgoing.set_header(header::HOST, addr.clone());
+
+            outgoing
+                .finish()
+                .unwrap()
                 .send()
                 .map_err(Error::from)          // <- convert SendRequestError to an Error
-                .and_then(
-                    |resp| {
-                        println!("proxy RESP: {:?}", resp.headers());
-                        resp.body()         // <- this is MessageBody type, resolves to complete body
-                            .from_err()            // <- convert PayloadError to an Error
-                            .and_then(move |body| {     // <- we got complete body, now send as server response
-                                let mut outgoing = HttpResponse::Ok();
-                                for (key, value) in resp.headers() {
-                                    outgoing.header(key.clone(), value.clone());
-                                }
-                                Ok(outgoing.body(body))
-                            })
-                    })
+                .and_then(|resp| {
+                    println!("proxy RESP: {:#?}", resp.headers());
+                    resp.body()         // <- this is MessageBody type, resolves to complete body
+                        .from_err()            // <- convert PayloadError to an Error
+                        .and_then(move |body| {     // <- we got complete body, now send as server response
+                            let mut outgoing = HttpResponse::Ok();
+                            for (key, value) in resp.headers() {
+                                outgoing.header(key.clone(), value.clone());
+                            }
+                            Ok(outgoing.body(body))
+                        })
+                })
                 .responder()
         });
     });
 
-    let request = proxy.get().uri(proxy.url("/")).finish().unwrap();
-    let response = proxy.execute(request.send()).unwrap();
+    let request = proxy.get()
+        .uri(proxy.url("/"))
+        .finish()
+        .unwrap();
 
-    let bytes = proxy.execute(response.body()).unwrap();
+    let response = proxy.execute(request.send()).unwrap();
+    let _bytes = proxy.execute(response.body()).unwrap();
 
     let has_header = response.headers().get("shane").unwrap();
+
     assert_eq!(has_header, "kittens");
 }
