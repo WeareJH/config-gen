f58fa6cebd1c1e65bffd53ad5c8dd58dfaec4690
Shane Osbourne <shane.osbourne8@gmail.com>
2018-08-08 22:58:53 +0100

    sending post requests

--- src/fns.rs
+++ src/fns.rs
@@ -8,15 +8,17 @@ use futures::future::{Either, ok};
 use rewrites::replace_host;
 use options::ProxyOpts;
 use std::str;
+use actix_web::http::HeaderMap;
 
 ///
 /// This function will clone incoming requests
 /// and pass them onto a backend specified via the `target` field on [ProxyOpts]
 ///
-pub fn proxy_transform(_req: &HttpRequest<ProxyOpts>) -> Box<Future<Item=HttpResponse, Error=Error>> {
+pub fn proxy_transform(_req: &HttpRequest<ProxyOpts>) -> Box<Future<Item = HttpResponse, Error = Error>> {
 
     // building up the new request that we'll send to the backend
     let mut outgoing = client::ClientRequest::build_from(_req);
+    let req_headers = _req.headers().clone();
 
     // build up the next outgoing URL (for the back-end)
     let next_url = format!("{}://{}{}{}",
@@ -45,73 +47,80 @@ pub fn proxy_transform(_req: &HttpRequest<ProxyOpts>) -> Box<Future<Item=HttpRes
     // then we need to buffer the body into memory in order to apply regex's on the string
     let next_target = _req.state().target.clone();
     let next_host = _req.uri().clone();
+    let original_method = _req.method().as_str().clone();
 
-    let next = match _req.method().as_str() {
-        "POST" => outgoing.body("here"),
-        _ => outgoing.finish()
-    };
+    if original_method == "POST" {
+        let outgoing = _req.body()
+            .from_err()
+            .and_then(move |incoming_body| {
 
-    _req.body()
-        .from_err()
-        .and_then(move |b| {
-            next
-                .unwrap()
-                .send()
-                .map_err(Error::from)
-                .and_then(move |resp| {
+                outgoing.body(incoming_body).unwrap().send().map_err(Error::from)
+                    .and_then(move |proxy_response| {
+                        println!("POST resp from proxy {:?}", &proxy_response);
+                        proxy_response.body()
+                            .from_err()
+                            .and_then(move |proxy_response_body| {
+                                Ok(create_outgoing(&proxy_response.headers()).body(proxy_response_body))
+                            })
+                    })
+            });
 
-                    // Should we rewrite this response?
-                    // just check for the correct content-type header for now.
-                    // This will need fleshing out to provide stricter checks
-                    let rewrite_response = match resp.headers().get(header::CONTENT_TYPE) {
-                        Some(t) => {
-                            match t.to_str().unwrap_or("") {
-                                "text/html" | "text/html; charset=UTF-8" => true,
-                                _ => false,
-                            }
+        Box::new(outgoing)
+    } else {
+        outgoing.finish().unwrap().send().map_err(Error::from)
+            .and_then(move |resp| {
+                // Should we rewrite this response?
+                // just check for the correct content-type header for now.
+                // This will need fleshing out to provide stricter checks
+                let rewrite_response = match resp.headers().get(header::CONTENT_TYPE) {
+                    Some(t) => {
+                        match t.to_str().unwrap_or("") {
+                            "text/html" | "text/html; charset=UTF-8" => true,
+                            _ => false,
                         }
-                        _ => false
-                    };
-
-                    // If we decide to modify the response, we need to buffer the entire
-                    // response into memory (text files only)
-                    if rewrite_response {
-                        Either::A(
-                            resp.body()
-                                .from_err()
-                                .and_then(move |body| {
-
-                                    use std::str;
-
-                                    // In here, we now have a ful buffered response body
-                                    // so we can go ahead and apply URL replacements
-                                    let req_host = next_host.host().unwrap_or("");
-                                    let req_port = next_host.port().unwrap_or(80);
-                                    let next_body = replace_host(
-                                        str::from_utf8(&body[..]).unwrap(),
-                                        &next_target,
-                                        req_host, req_port,
-                                    );
-                                    let as_string = next_body.to_string();
-                                    Ok(create_outgoing(&resp).body(as_string))
-                                })
-                        )
-                    } else {
-                        // If we get here, we decided not to re-write the response
-                        // so we just stream it back to the client
-                        Either::B(
-                            ok(create_outgoing(&resp).body(Body::Streaming(Box::new(resp.payload().from_err()))))
-                        )
                     }
-                })
-                .responder()
-        })
+                    _ => false
+                };
+
+                // If we decide to modify the response, we need to buffer the entire
+                // response into memory (text files only)
+                if rewrite_response {
+                    Either::A(
+                        resp.body()
+                            .from_err()
+                            .and_then(move |body| {
+
+                                use std::str;
+
+                                // In here, we now have a ful buffered response body
+                                // so we can go ahead and apply URL replacements
+                                let req_host = next_host.host().unwrap_or("");
+                                let req_port = next_host.port().unwrap_or(80);
+                                let next_body = replace_host(
+                                    str::from_utf8(&body[..]).unwrap(),
+                                    &next_target,
+                                    req_host, req_port,
+                                );
+                                let as_string = next_body.to_string();
+                                Ok(create_outgoing(&resp.headers()).body(as_string))
+                            })
+                    )
+                } else {
+                    // If we get here, we decided not to re-write the response
+                    // so we just stream it back to the client
+                    Either::B(
+                        ok(create_outgoing(&resp.headers()).body(Body::Streaming(Box::new(resp.payload().from_err()))))
+                    )
+                }
+            })
+            .responder()
+    }
 }
 
-fn create_outgoing(client_response: &client::ClientResponse) -> dev::HttpResponseBuilder {
+fn create_outgoing(req_headers: &HeaderMap) -> dev::HttpResponseBuilder {
     let mut outgoing = HttpResponse::Ok();
     // Copy headers from backend response to main response
-    for (key, value) in client_response.headers() {
+    for (key, value) in req_headers {
         outgoing.header(key.clone(), value.clone());
     }
     outgoing
@@ -164,9 +173,9 @@ mod tests {
         println!("main resp: {:#?}", response.headers());
         println!("bytes={:#?}", _bytes);
 
-        let has_header = response.headers().get("shane").unwrap();
+        let has_header = response.headers().get("shane").is_some();
 
-        assert_eq!(has_header, "kittens");
+        assert_eq!(has_header, true);
     }
 
     #[test]
@@ -181,11 +190,10 @@ mod tests {
                 assert_eq!(req.headers().get(header::ACCEPT).unwrap(), "text/html");
                 req.body()
                     .and_then(move |bytes: Bytes| {
-                        println!("{:#?}", bytes);
                         Ok(
                             HttpResponse::Ok()
                                 .header(header::CONTENT_TYPE, "application/json")
-                                .body(str::from_utf8(&bytes[..]).unwrap().to_string())
+                                .body(format!("REC-->{}", str::from_utf8(&bytes[..]).unwrap().to_string()))
                         )
                     })
                     .responder()
@@ -214,5 +222,7 @@ mod tests {
 
         println!("main resp: {:#?}", response.headers());
         println!("bytes={:#?}", _bytes);
+
+        assert_eq!(_bytes, r#"REC-->{"hello": "world"}"#)
     }
 }
